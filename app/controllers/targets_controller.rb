26c366ba99833da050ef9af74faaef4dd075e6ca
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2022-09-23 20:44:40 +0900

    annualの新規登録処理作成

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -4,9 +4,24 @@ class TargetsController < ApplicationController
     @targets = AnnualTarget.where(year: @year)
   end
 
+  def new
+    @target = AnnualTarget.new
+  end
+
+  def create
+    @target = AnnualTarget.new(target_params)
+    @target.save
+  end
+
   def month
     @year = params[:target_id] || Time.zone.now.year
     @month = params[:id] || Time.zone.now.month
     @targets = MonthlyTarget.where(year: @year, month: @month)
+  end
+
+  private
+
+  def target_params
+    params.require(:target).permit(:year, :name)
   end
 end
